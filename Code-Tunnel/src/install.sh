c30e2b6362e880fafea23c4560660a160f134d63
DaHofa02 <65934943+DaHofa02@users.noreply.github.com>
2024-01-31 22:12:27 +0100

    Update install.sh

--- Code-Tunnel/src/install.sh
+++ Code-Tunnel/src/install.sh
@@ -1,6 +1,6 @@
 apt update && upgrade -y
 apt install docker-compose && curl && git -y
-docker build -t root/code https://github.com/DaHofa02/cerberus.git#main:Code-Tunnel
+docker build -t root/code https://github.com/DaHofa02/viserion.git#main:Code-Tunnel
 mkdir code
-wget https://raw.githubusercontent.com/DaHofa02/cerberus/main/Code-Tunnel/docker-compose.yml -P ./code/
+wget https://raw.githubusercontent.com/DaHofa02/viserion/main/Code-Tunnel/docker-compose.yml -P ./code/
 docker-compose -f ./code/docker-compose.yml up -d
