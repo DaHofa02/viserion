cb5d787d230aa880fd5b1dc3fa93c43f9ac620af
Dahofa02 <marcel-hofer@hotmail.com>
2025-06-08 19:35:37 +0200

    added navbar

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeSwitch } from "@/components/ui/viserion/theme-switch"
+import { Navbar } from "@/components/ui/viserion/navbar"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Viserion",
+  description: "Viserion Landingpage by DaHofa",
+};
+
+export default function RootLayout(
+  { children, }: Readonly<{ children: React.ReactNode; }>
+) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
